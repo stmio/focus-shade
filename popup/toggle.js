680a3e5d64cac8a379c87599ed86505161ec851a
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-06-01 13:01:06 +0100

    feat: toggle extension status and save in chrome storage
    Add extension popup menu which allows users to enable/disable the extension. This is stored in
    Chrome storage so the status persists for future sessions.
    
    fix #1

---
+++ popup/toggle.js
@@ -0,0 +1,28 @@
+function initPopup() {
+  const statusText = document.getElementById("status");
+
+  chrome.storage.local.get(["isEnabled"]).then((result) => {
+    statusText.textContent = result.isEnabled ? "Enabled" : "Disabled";
+    if (result.isEnabled === false) statusText.classList.add("disabled");
+  });
+
+  document.getElementById("toggle").addEventListener("click", toggle);
+}
+
+function toggle() {
+  const statusText = document.getElementById("status");
+
+  if (statusText.textContent === "Enabled") {
+    statusText.textContent = "Disabled";
+    statusText.classList.add("disabled");
+  } else {
+    statusText.textContent = "Enabled";
+    statusText.classList.remove("disabled");
+  }
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { msg: "toggle-focus-mode" });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => initPopup());
--- yt-focus.js
+++ yt-focus.js
@@ -1,28 +1,57 @@
+function modifyPage() {
+  const elems = document.querySelectorAll(
+    "#contents, #chips, #related, .ytd-reel-shelf-renderer"
+  );
+
+  if (enabled) elems.forEach((elem) => elem.classList.remove("show"));
+  if (!enabled) elems.forEach((elem) => elem.classList.add("show"));
+}
+
 function addLocationObserver(callback) {
   const config = { attributes: false, childList: true, subtree: true };
   const observer = new MutationObserver(callback);
   observer.observe(document.body, config);
 }
 
-function observerCallback() {
-  const grids = document.querySelectorAll(
+function observer() {
+  const elems = document.querySelectorAll(
     "#contents, #chips, #related, .ytd-reel-shelf-renderer"
   );
+
+  if (!enabled) {
+    elems.forEach((elem) => elem.classList.add("show"));
+    return;
+  }
 
   if (
     window.location.href.includes("youtube.com/@") ||
     window.location.href.includes("youtube.com/results?")
   ) {
-    grids.forEach((grid) => {
-      grid.classList.add("show");
+    elems.forEach((elem) => {
+      elem.classList.add("show");
     });
     return;
   } else if (window.location.href.includes("youtube.com")) {
-    grids.forEach((grid) => {
-      grid.classList.remove("show");
+    elems.forEach((elem) => {
+      elem.classList.remove("show");
     });
   }
 }
 
-addLocationObserver(observerCallback);
-observerCallback();
+chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
+  if (data.msg === "toggle-focus-mode") {
+    enabled = !enabled;
+    chrome.storage.local.set({ isEnabled: enabled });
+    modifyPage();
+  }
+});
+
+let enabled;
+chrome.storage.local.get(["isEnabled"]).then((result) => {
+  enabled = result.isEnabled;
+});
+
+chrome.storage.local.set({ isEnabled: enabled });
+
+addLocationObserver(observer);
+observer();
