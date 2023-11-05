5e06046c2a5ddf21fe146714c1e26ee058db0b5b
Sam Turner <98767222+stmio@users.noreply.github.com>
2023-11-05 17:54:19 +0000

    feat: add basic session functionality

--- src/js/focus.js
+++ src/js/focus.js
@@ -38,11 +38,33 @@ function observer() {
   }
 }
 
+function startTimer(duration) {
+  sessionEnd = new Date(Date.now() + duration * 60000);
+  chrome.storage.sync.set({ sessionEnd: sessionEnd.toJSON() });
+  // const dt = 10000;
+  // await new Promise((resolve) => {
+  //   const interval = setInterval(() => {
+  //     if (Date.now() - sessionEnd > 0) {
+  //       resolve();
+  //       clearInterval(interval);
+  //     }
+  //   }, dt);
+  // });
+}
+
+let sessionEnd;
+chrome.storage.sync.get(["sessionEnd"]).then((result) => {
+  sessionEnd = new Date(result.sessionEnd);
+});
+
 chrome.runtime.onMessage.addListener((data, sender, sendResponse) => {
   if (data.msg === "toggle-focus-mode") {
     enabled = !enabled;
     chrome.storage.sync.set({ isEnabled: enabled });
+    if (data.duration) startTimer(data.duration);
     modifyPage();
+  } else if (data.msg === "get-session-timer") {
+    sendResponse({ end: sessionEnd });
   }
 });
 
@@ -50,7 +72,6 @@ let enabled;
 chrome.storage.sync.get(["isEnabled"]).then((result) => {
   enabled = result.isEnabled;
 });
-
 chrome.storage.sync.set({ isEnabled: enabled });
 
 addLocationObserver(observer);
--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,9 +1,31 @@
-function setStatusText() {
+function init() {
   const statusText = document.getElementById("status");
+  const options = document.getElementById("options");
+  const toggleButton = document.getElementById("toggle");
 
   chrome.storage.sync.get(["isEnabled"]).then((result) => {
     statusText.textContent = result.isEnabled ? "Enabled" : "Disabled";
     if (!result.isEnabled) statusText.classList.add("disabled");
+    else options.classList.add("hide");
+  });
+
+  chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+    try {
+      const res = await chrome.tabs.sendMessage(tabs[0].id, {
+        msg: "get-session-timer",
+      });
+
+      const end = new Date(res.end);
+      if (Date.now() - end < 0) {
+        toggleButton.classList.add("hide");
+        document.getElementById("timer").classList.remove("hide");
+        document.getElementById("mins").textContent = Math.round(
+          (end - Date.now()) / 60000
+        );
+      } else toggleButton.classList.remove("hide");
+    } catch (error) {
+      console.log("Extension only works on the YouTube website!");
+    }
   });
 
   document.getElementById("toggle").addEventListener("click", toggle);
@@ -11,22 +33,34 @@ function setStatusText() {
 
 function toggle() {
   const statusText = document.getElementById("status");
+  const options = document.getElementById("options");
+  const duration = document.getElementById("session-duration").value;
+  const toggleButton = document.getElementById("toggle");
 
   if (statusText.textContent === "Enabled") {
     statusText.textContent = "Disabled";
     statusText.classList.add("disabled");
+    options.classList.remove("hide");
   } else {
     statusText.textContent = "Enabled";
     statusText.classList.remove("disabled");
+    options.classList.add("hide");
+  }
+
+  if (duration) {
+    toggleButton.classList.add("hide");
   }
 
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
     try {
-      await chrome.tabs.sendMessage(tabs[0].id, { msg: "toggle-focus-mode" });
+      await chrome.tabs.sendMessage(tabs[0].id, {
+        msg: "toggle-focus-mode",
+        duration: duration,
+      });
     } catch (error) {
       console.log("Extension only works on the YouTube website!");
     }
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => setStatusText());
+document.addEventListener("DOMContentLoaded", () => init());
